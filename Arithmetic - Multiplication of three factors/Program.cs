ecf03b31f4d9990036f9511c7ecfdeed8e172c8c
Aidar Salakhov <adr-sal@yandex.ru>
2022-05-31 11:58:27 +0300

    Умножение 3х чисел

---
+++ Arithmetic - Multiplication of three factors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic___Multiplication_of_three_factors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{x * y * z}");
+        }
+    }
+}
