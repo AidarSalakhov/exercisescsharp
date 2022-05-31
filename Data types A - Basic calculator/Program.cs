c467b793927b45c7bb2e51473eb1d7d2da5962d0
Aidar Salakhov <adr-sal@yandex.ru>
2022-05-31 15:25:35 +0300

    Переименовал уроки

---
+++ Data types A - Basic calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_types_A___Basic_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{x} + {y} = {x+y}");
+            Console.WriteLine($"{x} - {y} = {x-y}");
+            Console.WriteLine($"{x} * {y} = {x*y}");
+            Console.WriteLine($"{x} / {y} = {x/y}");
+            Console.WriteLine($"{x} mod {y} = {x%y}");
+        }
+    }
+}
---
+++ Data types A - Reverse chars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_types_A___Reverse_chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
