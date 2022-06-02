3d6953447b56ff86a6c34532387656d2adbac7c0
Aidar Salakhov <adr-sal@yandex.ru>
2022-06-02 16:49:28 +0300

    TimeSpan

--- 019. Data types A - Float value/Program.cs
+++ 019. Data types A - Float value/Program.cs
@@ -10,7 +10,25 @@ namespace _019.Data_types_A___Float_value
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Input distance in meters");
+            float meters = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("Input time in hours");
+            int hours = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input time in minutes");
+            int minutes = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input time in seconds");
+            int seconds = int.Parse(Console.ReadLine());
+
+            TimeSpan timespan = new TimeSpan(hours, minutes, seconds);
+
+            Console.WriteLine($"Speed in meters/sec is {meters / timespan.TotalSeconds}");
+
+            Console.WriteLine($"Speed in km/h is {(meters/1000) / timespan.TotalHours}");
+
+            Console.WriteLine($"Speed in miles/h is {(meters/1609) / timespan.TotalHours}");
         }
     }
 }
